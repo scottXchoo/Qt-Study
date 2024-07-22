9a0883074b2ace124fc96f1aba161c65f0d89f04
scottXchoo <ckh0601@cau.ac.kr>
2024-07-22 17:06:48 +0900

    qt6_ep3

---
+++ qt6_ep3/include/test.h
@@ -0,0 +1,12 @@
+//
+// Created by 추교현 on 2024. 7. 22..
+//
+
+#ifndef QT6_EP3__TEST_H_
+#define QT6_EP3__TEST_H_
+
+class Test {
+
+};
+
+#endif //QT6_EP3__TEST_H_
---
+++ qt6_ep3/src/test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 추교현 on 2024. 7. 22..
+//
+
+#include "../include/test.h"
